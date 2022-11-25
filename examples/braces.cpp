f26b6593e8ba8d1def06c9e68b555c1e316be6a6
thedeepestreality <zetoster@gmail.com>
2022-11-25 12:56:45 +0300

    template tester

--- examples/braces.cpp
+++ examples/braces.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-bool check(const char str[]){
+typedef const char str16[16];
+
+bool check(const char* str){
     int counter = 0;
     int idx = 0;
     while (str[idx] != '\0'){
@@ -14,35 +16,52 @@ bool check(const char str[]){
     return counter == 0;
 }
 
-bool testCase(const char str[], bool expected, bool& res){
-    res = check(str);
-    return res == expected;
+//shortest-ever braces validator
+int c(char*s,int c){while(*s&&c>=0)c-=*s++*2-81;return!c;}
+
+template <class Type>
+struct CaseData{
+    Type input;
+    bool expected;    
+};
+
+template <class Type>
+bool testCase(
+    bool(*func)(Type),
+    CaseData<Type> data,
+    bool& res
+){
+    res = func(data.input);
+    return res == data.expected;
 }
 
 int main()
 {
-    const char* str_cases[] = {
-        "",
-        "(",
-        ")",
-        "()", 
-        ")(",
-        "()()",
-        "(()",
-        "())",
-        "(())",
-        ")(()))",
-        "(())((()())())"
+    CaseData<str16> test_cases[] = {
+        {"", true},
+        {"(", false},
+        {")", false},
+        {"()",true}, 
+        {")(", false},
+        {"()()", true},
+        {"(()", false},
+        {"())", false},
+        {"(())", true},
+        {")(()))", false},
+        {"(())((()())())", true}
     };
-    const size_t cNumCases = sizeof(str_cases)/sizeof(char*);
-    bool expected[cNumCases] = {true, false, false, true, false, true, false, false, true, false, true};
+    const size_t cNumCases = sizeof(test_cases)/sizeof(CaseData<str16>);
     bool result = true;
     for (size_t iter=0; iter < cNumCases; ++iter)
     {
         bool curr_check;
-        bool curr_test = testCase(str_cases[iter], expected[iter], curr_check);
-        std::cout << "test \"" << str_cases[iter] << "\" is " << (curr_test ? "ok" : "failed") << '\n';
-        std::cout << "\tresult: " << curr_check << " expected " << expected[iter] << '\n';
+        bool curr_test = testCase<str16>(
+            check, 
+            test_cases[iter], 
+            curr_check
+        );
+        std::cout << "test \"" << test_cases[iter].input << "\" is " << (curr_test ? "ok" : "failed") << '\n';
+        std::cout << "\tresult: " << curr_check << " expected " << test_cases[iter].expected << '\n';
         result = result && curr_test;
     }
     std::cout << "Total result: " << result << '\n';
