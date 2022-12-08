bbd016ec92418776ef6c0ef15a9a39aed665d0d8
thedeepestreality <zetoster@gmail.com>
2022-12-08 16:58:40 +0300

    autolevels bmp color balancing

--- examples/bmp_img/bmp.cpp
+++ examples/bmp_img/bmp.cpp
@@ -39,6 +39,12 @@ int main(){
     std::cout << "img size: " << img_size << std::endl;
     // in.seekg(16, std::ios::cur);
 
+    unsigned char b_min = 255;
+    unsigned char b_max = 0;
+    unsigned char g_min = 255;
+    unsigned char g_max = 0;
+    unsigned char r_min = 255;
+    unsigned char r_max = 0;
     std::ofstream out("out.bmp", std::ios::binary);
     in.seekg(0, std::ios::beg);
     in.read((char*)buf, 54);
@@ -47,6 +53,21 @@ int main(){
         for (int col = 0; col < width; ++col)
         {
             in.read((char*)buf, 3);
+            if (buf[0] > b_max) b_max = buf[0];
+            if (buf[0] < b_min) b_min = buf[0];
+            if (buf[1] > g_max) g_max = buf[1];
+            if (buf[1] < g_min) g_min = buf[1];
+            if (buf[2] > r_max) r_max = buf[2];
+            if (buf[2] < r_min) r_min = buf[2];
+        }
+    in.seekg(54, std::ios::beg);
+    for (int row = 0; row < height; ++row)
+        for (int col = 0; col < width; ++col)
+        {
+            in.read((char*)buf, 3);
+            buf[0] = (buf[0] - b_min)*255/(b_max-b_min);
+            buf[1] = (buf[1] - g_min)*255/(g_max-g_min);
+            buf[2] = (buf[2] - r_min)*255/(r_max-r_min);
             out.write((char*)buf, 3);
         }
     in.close();
