50f4cd88c9738d17fd899acc20bc2071e56c4918
Ruslan Sevostyanov <zetoster@gmail.com>
2024-11-16 14:26:03 +0300

    updated cpp linux serial example

--- sem3/serial/linux.cpp
+++ sem3/serial/linux.cpp
@@ -10,7 +10,7 @@
 
 int main() {
   // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
-  int serial_port = open("/dev/ttyS5", O_RDWR);
+  int serial_port = open("/dev/ttyUSB0", O_RDWR);
 
   // Create new termios struct, we call it 'tty' for convention
   struct termios tty;
@@ -23,7 +23,7 @@ int main() {
 
   tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
   tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
   tty.c_cflag |= CS8; // 8 bits per byte (most common)
   tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
   tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
@@ -41,12 +41,12 @@ int main() {
   // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
   // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
 
-  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VTIME] = 20;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
   tty.c_cc[VMIN] = 0;
 
-  // Set in/out baud rate to be 115200
-  cfsetispeed(&tty, B115200);
-  cfsetospeed(&tty, B115200);
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B9600);
+  cfsetospeed(&tty, B9600);
 
   // Save tty settings, also checking for error
   if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
@@ -54,24 +54,24 @@ int main() {
       return 1;
   }
 
+  sleep(2);
+
   // Write to serial port
-  unsigned char msg[] = { '1' };
-  printf("before write\n");
-  write(serial_port, msg, sizeof(msg));
-  printf("written!\n");
+  unsigned char msg[] = "100";
+  write(serial_port, msg, 3);
+
   // Allocate memory for read buffer, set size according to your needs
-  char read_buf [1];
+  char read_buf [256];
 
   // Normally you wouldn't do this memset() call, but since we will just receive
   // ASCII data for this example, we'll set everything to 0 so we can
   // call printf() easily.
-  memset(read_buf, '\0', sizeof(read_buf));
+  memset(&read_buf, '\0', sizeof(read_buf));
 
   // Read bytes. The behaviour of read() (e.g. does it block?,
   // how long does it block for?) depends on the configuration
   // settings above, specifically VMIN and VTIME
-  printf("before read\n");
-  int num_bytes = read(serial_port, read_buf, 1);
+  int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
 
   // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
   if (num_bytes < 0) {
